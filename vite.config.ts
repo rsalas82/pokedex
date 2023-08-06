5b5ab3296ebf9cd3ec82ce669e214e83b1ab321c
Rafa Salas <rafsalrob@gmail.com>
2023-08-06 10:33:12 +0200

    tests: basic testing config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   server: {
     port: 3000,
   },
