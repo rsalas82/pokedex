c588b10c738016ba750c6a0542943bcf4697a514
Rafa Salas <rafsalrob@gmail.com>
2023-08-07 16:56:54 +0200

    fix: rename files

---
+++ src/pages/Pokedex.ttt.tsx
@@ -0,0 +1,11 @@
+// import { describe, expect, test } from 'vitest'
+// import {render, screen, waitFor} from '@testing-library/react'
+// import Pokedex from './Pokedex'
+
+
+// describe('Pokedex', () => {
+//     test('should render the home page', async() => {
+//         render(<Pokedex />)
+//         await waitFor(() => {expect(screen.getByText(/voltorb/i))})
+//     })
+// })
